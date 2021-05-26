04dbaa85d44536dab6f1029572cc8c6bcd4eedc5
Lucas Martini <kpolucas88@gmail.com>
2021-05-26 16:12:41 -0300

    boss teleport mechanic

--- Scripts/Boss.gd
+++ Scripts/Boss.gd
@@ -7,40 +7,48 @@ onready var bossSpriteMaterial = $BossSprite.material
 onready var bossAnimationTree = $BossAnimationTree.get("parameters/playback")
 onready var Player = get_node("/root/Main/World/Player")
 
+func _ready():
+	randomize()
+
 func _process(delta):
 	if health <= 0:
 		queue_free()
 
-	_flashDecay()
+	_flash_decay()
 
 		
-func _flashStart():
+func _flash_start():
 	bossSpriteMaterial.set_shader_param("flashIntensity" ,1.0)
 	flashI = 1.0
-func _flashDecay():
+func _flash_decay():
 	if flashI >= 0:
 		flashI -= 0.1
 		bossSpriteMaterial.set_shader_param("flashIntensity" , flashI)
-
-func _teleport():
-	bossAnimationTree.travel("teleportOut")
 	
 func _act():
 	pass
 
 func OnHit(damage):
 	health -= damage
-	_flashStart()
+	_flash_start()
 	# $DamageParticles.emitting = true # TODO
 	
 	print("Enemy health: " + str(health))
 
+func _teleport():
+	var actions = ["attack1","attack2","laugh"]
+	actions.shuffle()
+	bossAnimationTree.travel(actions[1])
+	$AttackCooldown.start(5)
+	
+func move_behind_player(): # se dispara desde animationPlayer teleportOut
+	var _playerPos = Player.get_position().x
+	var _playerFacingRight = Player.facing_right
+	
+	var offSet = -25 if _playerFacingRight else 25
+	$BossSprite.scale.x = -1 if _playerFacingRight else 1
+	
+	self.set_position(Vector2(_playerPos + offSet , self.get_position().y)) # Desarmar en algo menos horrible
+	
 func _on_AttackCooldown_timeout():
 	_teleport()
-	$AttackCooldown.start(3)
-#	var _deltaPos = abs(self.get_position().x - Player.get_position().x)
-#	if _deltaPos < 150:
-#		bossAnimationTree.travel("attack1")
-#	else:
-#		bossAnimationTree.travel("teleport")
-#		self.set_position(Vector2(Player.get_position().x , self.get_position().y)) # Desarmar en algo menos horrible
